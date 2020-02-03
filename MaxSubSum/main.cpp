652ac6724e7921ff2bec96363642a2f72e6152c1
Diego Olvera <olveram.diego@outlook.com>
2020-02-03 11:46:41 -0600

    Function createCsvFile added. Main program can read and print .csv file content. Dataset with 10 rows and 11 col.

--- MaxSubSum/main.cpp
+++ MaxSubSum/main.cpp
@@ -9,19 +9,23 @@
 #include <iostream>
 #include <fstream> // Biblioteca para manipular archivos
 #include <string> // Biblioteca para cadenas de texto
-#include <time.h>
+#include <time.h> // Biblioteca para tiempos
+#include <sstream> // Biblioteca para manipular cadenas de textos
+#include <vector> // Biblioteca para arreglos
 
 using namespace std;
 
-int main() {
+// Función que genera el dataset
+void createCsvFile() 
+{
 	fstream file;
 
 	// Inicializar la semilla aleatoria
 	srand(time(NULL));
 
 	// Abrir o crear un archivo .csv
-	file.open("dataset1.csv",ios::out);
-	
+	file.open("dataset1.csv", ios::out);
+
 	// Escribir enteros aleatorios en el archivo
 	for (int i = 0; i < 10; i++)
 	{
@@ -36,10 +40,44 @@ int main() {
 		file << '\n';
 	}
 	file.close();
+}
+
+
+int main() {
+	
+	string cell;
+	vector<int> parsedRow;
+	vector<vector<int>> parsedCsv;
+	
+	// Ejecutar solo una vez
+	// createCsvFile();
 
 	// Leer un archivo .csv y guardar su contenido en arreglos
 
+	ifstream data("dataset1.csv");
+	string line;
+	while (std::getline(data,line))
+	{
+		stringstream dataStream(line);
+		while (getline(dataStream, cell, ','))
+		{
+			//cout << "Cell value is: " << cell << '\n';
+			parsedRow.push_back(stoi(cell));
+		}
+		parsedCsv.push_back(parsedRow);
+		//restart parsedRow vector
+		parsedRow.clear();
+	}
 
+	// Imprimir los elementos de la matriz
+	for (int i = 0; i < parsedCsv.size(); i++)
+	{
+		for (int j = 0; j < parsedCsv[i].size(); j++)
+		{
+			cout << parsedCsv[i][j] << ' ';
+		}
+		cout << endl;
+	}
 	// Ejecutar el algoritmo con complejidad O(n)
 
 	// Ejecutar el algoritmo con complejidad O(n log n)
